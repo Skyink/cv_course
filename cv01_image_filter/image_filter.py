60877ff00bf287a1c3c02e550f42d856733ec45d
Skyink <ctb328@126.com>
2024-03-10 03:43:38 +0800

    fix: hsv filter

--- cv01_image_filter/image_filter.py
+++ cv01_image_filter/image_filter.py
@@ -17,7 +17,8 @@ def my_gauss_kernel(size=3, sigma=1.5):
         for j in range(size):
             numerator = math.pow(i-size-1, 2) + math.pow(j-size-1, 2)
             # 求高斯卷积
-            my_kernel[i, j] = math.exp(-numerator/(2*math.pow(sigma, 2))) / (2*math.pi*sigma**2)
+            # my_kernel[i, j] = math.exp(-numerator/(2*math.pow(sigma, 2))) / (2*math.pi*sigma**2)
+            my_kernel[i, j] = math.exp(-numerator / (2 * math.pow(sigma, 2)))   # 最后要归一化，可以将系数计算省略
     sum = np.sum(my_kernel)     # 求和
     kernel = my_kernel/sum      # 归一化
     return kernel
@@ -39,6 +40,9 @@ def my_gauss_filter(img, ksize=3, sigma=1.5, kernel=[]):
                 for l in range(ksize_below, ksize_up):
                     sum += img[i + k, j + l] * kernel[k + 1, l + 1]  # 高斯滤波
             result_img[i, j] = sum
+        if i % 100 == half_ksize:
+            print("Calculating convolution... i={}".format(i))
+    print("Convolution is finished!")
     return result_img
 
 
@@ -69,12 +73,12 @@ def my_gauss_filter_hsv(img, ksize=3, sigma=1.5, kernel=None):
         kernel = my_gauss_kernel(ksize, sigma)
     # 获得HSV彩色图像，拆分通道
     img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-    img_h, img_s, img_v = cv.split(img)
+    img_h, img_s, img_v = cv.split(img_hsv)
     # 仅对V通道进行滤波
     ch_h = img_h
     ch_s = img_s
     ch_v = my_gauss_filter(img_v, ksize, sigma, kernel)
-    result_img_hsv = cv.merge([ch_h, ch_s, ch_v])
+    result_img_hsv = cv.merge((ch_h, ch_s, ch_v))
     # 转换回BGR色彩
     result_img = cv.cvtColor(result_img_hsv, cv.COLOR_HSV2BGR)
     return result_img
@@ -83,7 +87,7 @@ def my_gauss_filter_hsv(img, ksize=3, sigma=1.5, kernel=None):
 # ==== main ========================================================
 
 # 设置参数
-K_SIZE = 3
+K_SIZE = 5
 SIGMA = 5
 
 # 读取图像(彩色)
@@ -91,40 +95,45 @@ img = cv.imread('image/doge.png')
 # 处理为灰度图像
 img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-# # 计算卷积核
-# my_kernel = my_gauss_kernel(K_SIZE, SIGMA)
-#
-# # 处理灰度图像 img_gray
-# # 使用自定义高斯滤波函数
-# img_filter_gray = my_gauss_filter_single(img_gray, K_SIZE, SIGMA, my_kernel)
-# # 使用库函数
-# img_guassian_gray = cv.GaussianBlur(img_gray, (K_SIZE, K_SIZE), SIGMA, my_kernel)
-# # 对比灰度滤波效果
-# img_compare_gray = np.hstack([img_gray, img_filter_gray, img_guassian_gray])
+# 计算卷积核
+my_kernel = my_gauss_kernel(K_SIZE, SIGMA)
+
+# 处理灰度图像 img_gray
+# 使用自定义高斯滤波函数
+img_filter_gray = my_gauss_filter_single(img_gray, K_SIZE, SIGMA, my_kernel)
+# 使用库函数
+img_guassian_gray = cv.GaussianBlur(img_gray, (K_SIZE, K_SIZE), SIGMA, my_kernel)
+# 对比灰度滤波效果
+img_compare_gray = np.hstack([img_gray, img_filter_gray, img_guassian_gray])
+# 显示结果及保存
 # img_show('compare_gray', img_compare_gray)
-#
-# # 处理彩色图像 img
-# # 使用自定义高斯滤波函数(BGR)
-# img_filter_bgr = my_gauss_filter_bgr(img, K_SIZE, SIGMA, my_kernel)
-# # 使用自定义高斯滤波函数(HSV)
-# img_filter_hsv = my_gauss_filter_hsv(img, K_SIZE, SIGMA, my_kernel)
-# # 使用库函数
-# img_guassian_bgr = cv.GaussianBlur(img, (K_SIZE, K_SIZE), SIGMA)
-# # 对比彩色滤波效果
-# img_compare_color = np.hstack([img, img_filter_bgr, img_filter_hsv, img_guassian_bgr])
+cv.imwrite("image/img_compare_gray.png", img_compare_gray)
+
+# 处理彩色图像 img
+# 使用自定义高斯滤波函数(BGR)
+img_filter_bgr = my_gauss_filter_bgr(img, K_SIZE, SIGMA, my_kernel)
+# 使用自定义高斯滤波函数(HSV)
+img_filter_hsv = my_gauss_filter_hsv(img, K_SIZE, SIGMA, my_kernel)
+# 使用库函数
+img_guassian_bgr = cv.GaussianBlur(img, (K_SIZE, K_SIZE), SIGMA)
+# 对比彩色滤波效果
+img_compare_color = np.hstack([img, img_filter_bgr, img_filter_hsv, img_guassian_bgr])
+# 显示结果及保存
 # img_show('compare_color', img_compare_color)
+cv.imwrite("image/img_compare_color.png", img_compare_color)
 
 # 对比高斯核参数size
 img_filter_size3 = my_gauss_filter_bgr(img, 3, 5)
 img_filter_size5 = my_gauss_filter_bgr(img, 5, 5)
 img_filter_size9 = my_gauss_filter_bgr(img, 9, 5)
 compare_size = np.hstack([img, img_filter_size3, img_filter_size5, img_filter_size9])
-
 # 对比高斯核参数sigma
 img_filter_sigma2 = my_gauss_filter_bgr(img, 9, 2)
 img_filter_sigma5 = my_gauss_filter_bgr(img, 9, 5)
 img_filter_sigma10 = my_gauss_filter_bgr(img, 9, 10)
 compare_sigma = np.hstack([img, img_filter_sigma2, img_filter_sigma5, img_filter_sigma10])
 image_compare_kernel = np.vstack([compare_size, compare_sigma])
-img_show('image_compare_kernel', image_compare_kernel)
+# 显示结果及保存
+# img_show('image_compare_kernel', image_compare_kernel)
+cv.imwrite("image/image_compare_kernel.png", image_compare_kernel)
 
